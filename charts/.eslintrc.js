54c752bad1ddd49304ffd83613121a174b14e560
Timur Yermakou <timur.ermakov@fortegrp.com>
2021-01-02 17:08:03 +0300

    init: start project

---
+++ charts/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  parser: 'babel-eslint',
+    extends: [
+      'airbnb-typescript',
+      'airbnb/hooks',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:jest/recommended',
+      'prettier',
+      'prettier/react',
+      'prettier/@typescript-eslint',
+      'plugin:prettier/recommended'
+    ],
+    plugins: ['react', '@typescript-eslint', 'jest'],
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
+    },
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 2018,
+      sourceType: 'module',
+      project: './tsconfig.json',
+    },
+    overrides: [
+      {
+        files: ['**/*.ts?(x)'],
+        parser: '@typescript-eslint/parser',
+        plugins: ['@typescript-eslint'],
+      }
+    ],
+    rules: {
+      'linebreak-style': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  };
